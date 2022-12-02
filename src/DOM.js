b3e03f13d99026b8703cea509c929c51215d9547
BCupen <bacupen@gmail.com>
2022-12-01 20:07:47 -0400

    sidebar built

--- src/DOM.js
+++ src/DOM.js
@@ -1,7 +1,26 @@
 
 const DOM = (() =>{
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const sidebar = document.querySelector('.sidebar');
+    const main = document.querySelector('.main');
     
+    function loadHandlers(){
+        let menuActive = false;
+        hamburgerMenu.addEventListener('click', (e)=>{
+            if(!menuActive){
+                sidebar.style.width = "15rem";
+                main.style.marginLeft = "15rem"; 
+                menuActive = true;
+            }else{
+                sidebar.style.width = "0";
+                main.style.marginLeft = "0";
+                menuActive = false;
+            }
+            
+        })
+    }
 
+    return {loadHandlers};
 })();
 
 export default DOM;
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import DOM from './DOM.js';
 import './style.css'
 
 console.log('here');
+DOM.loadHandlers();
